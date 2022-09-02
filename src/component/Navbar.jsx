90571e9b3679db7c22ab697095367dcdbfdb13e8
obenstein <hammadsami119@gmail.com>
2022-09-02 17:21:51 +0500

    .

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,91 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Search } from "@mui/icons-material";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-height: 60px;
-background-color: black;
-color: white;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  height: 60px;
+  background-color: lightgray;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  cursor: pointer;
+  font-size: 14px;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid gray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-      This is navbar
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Monarch.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+          <Badge badgeContent={19} color="primary">
+        <ShoppingCartOutlinedIcon />
+        </Badge>
+        </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
