0c81c3f501051dd71961915fb5862c3afdcb86aa
obenstein <hammadsami119@gmail.com>
2022-09-02 23:04:33 +0500

    .

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,44 +1,72 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material'
-import React from 'react'
-import styled from 'styled-components'
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-height: 100vh;
-width: 100%;
-display: flex;
-position: relative;
-background-color: peachpuff;
-`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  position: relative;
+  /* background-color: #c6002a; */
+`;
+
 const Arrow = styled.div`
-width: 50px;
-height: 50px;
-background-color: aliceblue;
-border-radius: 50%;
-display: flex;
-align-items: center;
-justify-content: center;
-position: absolute;
-top: 0;
-bottom: 0;
-left: ${props=>props.direction === "left" && "10px"};
-right: ${props=>props.direction === "right" && "10px"};
-margin: auto;
-cursor: pointer;
-opacity: 0.5;
-`
+  width: 50px;
+  height: 50px;
+  background-color: aliceblue;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const Slide = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+`;
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
-        
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://spng.pngfind.com/pngs/s/32-323913_womens-fashion-images-png-transparent-png.png" />
+          </ImgContainer>
+          <InfoContainer></InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
