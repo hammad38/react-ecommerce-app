dff12518cc881010e47664fb88444ddf81c6f7a4
obenstein <hammadsami119@gmail.com>
2022-09-02 16:05:43 +0500

    .

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 60px;
+background-color: black;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+`
 
 const Navbar = () => {
   return (
-    <div>
+    <Container>
       This is navbar
-    </div>
+    </Container>
   )
 }
 
