1d48f118ad50ea25760a244cec25c1af26013b52
obenstein <hammadsami119@gmail.com>
2022-09-06 17:18:13 +0500

    .

--- src/component/Categories.jsx
+++ src/component/Categories.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components"
-import { categories } from "../data"
+import  {categories}  from "../data"
 import CategoryItem from "./CategoryItem"
 
 const Container = styled.div`
+display: flex;
+padding: 20px;
+justify-content: space-between;
 `
 
 const Categories = () => {
--- src/component/CategoryItem.jsx
+++ src/component/CategoryItem.jsx
@@ -1,12 +1,38 @@
 import styled from "styled-components"
 
 const Container = styled.div`
+flex:1;
+margin: 3px;
+height: 70vh;
+`
+const Image = styled.img`
+height: 100%;
+width: 100%;
+object-fit: cover;
+`
+const Info = styled.div``
+const Title = styled.h1``
+
+const Button = styled.button`
+text-transform: uppercase;
+padding: 5px;
+/* margin: 10px; */
+background-color: transparent;
+font-size: 15px;
+border: 1px solid gray;
+cursor: pointer;
+
 `
 
 const CategoryItem = ({item}) => {
   return (
     <Container>
-
+        <Image src={item.img} />
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>shop now</Button>
+            </Info>
+        
     </Container>
   )
 }
