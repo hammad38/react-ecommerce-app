1da9224617d88a14af9dbc9ca2babd8ae9bd8d55
Hammad Shoaib <hammadsami119@gmail.com>
2022-09-07 21:40:02 +0500

    create newsletter

---
+++ src/component/Newsletter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const Newsletter = () => {
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default Newsletter
