d9f98106c47f8fb49c0074460319124b083055da
obenstein <hammadsami119@gmail.com>
2022-09-08 18:02:18 +0500

    .

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-
 `
-
+const Title = styled.h1``
+const Description = styled.div``
+const InputContainer = styled.div``
+const Input = styled.input``
+const Button = styled.button``
 const Newsletter = () => {
   return (
     <Container>
+    <Title>Newsletter</Title>
+    <Description></Description>
+    <InputContainer>
+      <Input />
+      <Button></Button>
+    </InputContainer>
     </Container>
   )
 }
