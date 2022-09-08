ce7982667c758fba7311158d0f0725ef9de064f5
obenstein <hammadsami119@gmail.com>
2022-09-08 18:49:01 +0500

    .

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,11 +2,24 @@ import styled from "styled-components"
 
 const Container = styled.div`
 `
+const Left = styled.div``
+
+const Center = styled.div`
+`
+const Right = styled.div`
+`
 
 const Footer = () => {
   return (
     <Container>
+      <Left>
+        <Logo>Monarch,</Logo>
+        <Decs>There are many variation of packages of Lorem Ispum available, but the majority have suffered alteration in some form, by injected humor, or randomised words which don't look even slightly believable</Decs>
 
+      </Left>
+      <Center></Center>
+      <Right></Right>
+      
     </Container>
   )
 }
