0d6e10947a7e2f0fa4ac3e050fa2f9617902a746
obenstein <hammadsami119@gmail.com>
2022-09-08 21:34:21 +0500

    update footer component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,79 +1,147 @@
-import { FacebookOutlined, Instagram, LinkedIn, Twitter } from "@mui/icons-material"
-import styled from "styled-components"
+import {
+  FacebookOutlined,
+  Instagram,
+  LinkedIn,
+  Mail,
+  Phone,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
 
 const Container = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 const Left = styled.div`
-flex: 1;
-display: flex;
-flex-direction: column;
-padding: 20px;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
 const Logo = styled.h1`
-cursor: pointer;
-display: flex;
-`
+  cursor: pointer;
+  display: flex;
+`;
 const Desc = styled.p`
-margin: 20px 0px;
-`
+  margin: 20px 0px;
+`;
 const SocialContainer = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 const SocialIcon = styled.div`
-cursor: pointer;
-width: 40px;
-height: 40px;
-border-radius: 50%;
-color: #fff;
-background-color: #${props=>props.color};
-display: flex;
-align-items: center;
-justify-content: center;
-margin-right: 20px;
-`
-
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
-flex: 1;
+  flex: 1;
+  padding: 20px;
+`;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
 
-`
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
-flex: 1;
+  flex: 1;
+  padding: 20px;
+`;
 
-`
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+width: 50% ;
+`;
 
 const Footer = () => {
   return (
     <Container>
       <Left>
         <Logo>Monarch.</Logo>
-        <Desc>There are many variation of packages of Lorem Ispum available, but the majority have suffered alteration in some form, by injected humor, or randomised words which don't look even slightly believable.</Desc>
+        <Desc>
+          There are many variation of packages of Lorem Ispum available, but the
+          majority have suffered alteration in some form, by injected humor, or
+          randomised words which don't look even slightly believable.
+        </Desc>
 
         <SocialContainer>
           <SocialIcon color="1877f2">
-            <FacebookOutlined  />
+            <FacebookOutlined />
           </SocialIcon>
 
-          <SocialIcon color= "c32aa3" >
+          <SocialIcon color="c32aa3">
             <Instagram />
           </SocialIcon>
 
-          <SocialIcon color = "1da1f2">
+          <SocialIcon color="1da1f2">
             <Twitter />
           </SocialIcon>
 
-          <SocialIcon color = "0072b1"> 
+          <SocialIcon color="0072b1">
             <LinkedIn />
           </SocialIcon>
         </SocialContainer>
-
       </Left>
-      <Center></Center>
-      <Right></Right>
-      
-    </Container>
-  )
-}
 
-export default Footer
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> Plot 8, Karachi Memon
+          Co-operative Housing Society Umar Colony, Karachi City, Sindh,
+          Pakistan
+        </ContactItem>
+
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +92-320-3011640
+        </ContactItem>
+
+        <ContactItem>
+          <Mail style={{ marginRight: "10px" }} /> contact@monarch.com
+        </ContactItem>
+
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
