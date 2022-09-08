ef78939d50f1f6671435d1b5b9895fa8653c07dc
obenstein <hammadsami119@gmail.com>
2022-09-08 17:14:47 +0500

    create footer component

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+`
+
+const Footer = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Footer
