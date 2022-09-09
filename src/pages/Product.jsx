5210a5bdeb7febc998e5ae5458f0ed26802a80a8
obenstein <hammadsami119@gmail.com>
2022-09-09 19:34:24 +0500

    adding image

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,21 +1,28 @@
-import styled from "styled-components"
-import Announcement from "../component/Announcement"
-import Footer from "../component/Footer"
-import Navbar from "../component/Navbar"
-import Newsletter from "../component/Newsletter"
+import styled from "styled-components";
+import Announcement from "../component/Announcement";
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
+import Newsletter from "../component/Newsletter";
 
-const Container = styled.div`
-`
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const ImgContainer = styled.div``;
+const Image = styled.img``;
 
 const Product = () => {
   return (
     <Container>
-        <Navbar />
-        <Announcement />
-        <Newsletter />
-        <Footer />
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
