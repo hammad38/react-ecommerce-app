c612fcea88dc7053bdb2e76dec0c472edd178aca
Hammad Shoaib <hammadsami119@gmail.com>
2022-09-12 20:45:49 +0500

    .

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,17 +9,13 @@ const Container = styled.div``;
 const Wrapper = styled.div``;
 const ImgContainer = styled.div``;
 const Image = styled.img``;
-const InfoContainer = styled.div``
-const Title = styled.h1``
-const Desc = styled.p``
-
-
+const InfoContainer = styled.div``;
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Price = styled.span``
 const Product = () => {
   return (
-    
-    
     <Container>
-    
       <Navbar />
       <Announcement />
       <Wrapper>
@@ -29,7 +25,13 @@ const Product = () => {
 
         <InfoContainer>
           <Title>Denim JumpSuit </Title>
-          <Desc>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae illum voluptatum fugit quod aut natus iure molestias vel! Ut hic maxime quidem possimus nam excepturi dolor tempore officiis consequuntur porro!</Desc>
+          <Desc>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae illum
+            voluptatum fugit quod aut natus iure molestias vel! Ut hic maxime
+            quidem possimus nam excepturi dolor tempore officiis consequuntur
+            porro!
+          </Desc>
+          <Price>$ 20</Price>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
