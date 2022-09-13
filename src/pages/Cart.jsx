d4e101c5d7287817e90bcf0ff92badcb7783f1da
obenstein <hammadsami119@gmail.com>
2022-09-13 23:14:25 +0500

    .

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,10 +3,14 @@ import Announcement from "../component/Announcement"
 import Footer from "../component/Footer"
 import Navbar from "../component/Navbar"
 
-const Container = styled.div``
-const Wrapper = styled.div``
+const Container = styled.div`  
+ `
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+`
 const Title = styled.h1``
-const Top = styled.div``
+const Top = styled.div``                 
 const Bottom = styled.div``
 
 
