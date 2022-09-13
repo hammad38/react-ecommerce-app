44b336671a0fe3af054556109d4bfbd962674e00
obenstein <hammadsami119@gmail.com>
2022-09-13 17:10:27 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 import "./style.css";
 
 const App = () => {
     return (
     <>
-    <Product />
+    <Register />
         </>
   );
 };
