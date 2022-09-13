0219603593bd2e151b2c4ca7014d5dd7a5de05d2
obenstein <hammadsami119@gmail.com>
2022-09-13 17:15:01 +0500

    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,17 @@ const Container = styled.div`
 const Register = () => {
   return (
     <Container>
-
+<Wrapper>
+    <Title>create an account</Title>
+    <Form>
+        <Input placeholder="name" />
+        <Input placeholder="last name" />
+        <Input placeholder="username" />
+        <Input placeholder="email" />
+        <Input placeholder="password" />
+        <Input placeholder="confirm password" />
+    </Form>
+</Wrapper>
     </Container>
   )
 }
