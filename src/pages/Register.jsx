c1abbf4bda5302ce2876b21e4b8a40b80ab9bd7f
obenstein <hammadsami119@gmail.com>
2022-09-13 18:40:45 +0500

    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,22 +10,43 @@ width: 100vw;
 display: flex;
 align-items: center;
 justify-content: center;
+position: relative;
 `;
 
 const Wrapper = styled.div`
-padding: 200px;
+width: 40%;
+padding: 20px;
+background-color: #fff;
 `;
 
 const Title = styled.h1`
   text-transform: uppercase;
+  font-size: 24px;
+  font-weight: 300;
 `;
-const Form = styled.form``;
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+flex: 1 ;
+min-width: 40%;
+margin: 10px 20px 0px 0px;
+padding: 7px;
+`;
 
-const Agreement = styled.span``
+const Agreement = styled.span`
+font-size: 12px;
+margin: 20px 0px;
+`
 
-const Button = styled.button``
+const Button = styled.button`
+display: flex;
+align-items: center;
+justify-content: center;
+/* position: absolute; */
+`
 
 const Register = () => {
   return (
@@ -39,7 +60,7 @@ const Register = () => {
           <Input placeholder="email" />
           <Input placeholder="password" />
           <Input placeholder="confirm password" />
-          <Agreement>By creating an account, I consent to the processing of my personal data in accordance with the </Agreement>
+          <Agreement>By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b> </Agreement>
           <Button>CREATE</Button>
         </Form>
       </Wrapper>
