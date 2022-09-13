393cd2ae2d5d239540549098e405fa8badf9a107
obenstein <hammadsami119@gmail.com>
2022-09-13 18:17:43 +0500

    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,50 @@
-import styled from "styled-components" 
+import styled from "styled-components";
 
-const Container = styled.div``
-const Wrapper = styled.div``
+const Container = styled.div`
+height: 100vh;
+width: 100vw;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Wrapper = styled.div`
+padding: 200px;
+`;
+
 const Title = styled.h1`
-text-transform: uppercase;
-`
-const Form = styled.form``
-const Input = styled.input``
+  text-transform: uppercase;
+`;
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Agreement = styled.span``
+
+const Button = styled.button``
 
 const Register = () => {
   return (
     <Container>
-<Wrapper>
-    <Title>create an account</Title>
-    <Form>
-        <Input placeholder="name" />
-        <Input placeholder="last name" />
-        <Input placeholder="username" />
-        <Input placeholder="email" />
-        <Input placeholder="password" />
-        <Input placeholder="confirm password" />
-    </Form>
-</Wrapper>
+      <Wrapper>
+        <Title>create an account</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>By creating an account, I consent to the processing of my personal data in accordance with the </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
