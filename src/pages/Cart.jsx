4d0fc5116a309bf0639bbb48f9af34aa508793be
obenstein <hammadsami119@gmail.com>
2022-09-14 17:03:02 +0500

    .

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { yellow } from "@mui/material/colors";
 import styled from "styled-components";
 import Announcement from "../component/Announcement";
 import Footer from "../component/Footer";
@@ -8,29 +9,34 @@ const Wrapper = styled.div`
   padding: 20px;
 `;
 const Title = styled.h1`
-text-align: center;
+  text-align: center;
 `;
 
 const Top = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
 `;
 
-const TopButton = styled.button``;
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  border: ${(props) => (props.type === "filled" && "none")};
+  background-color: ${props=>props.type === "filled" && "black"  };
+  color: ${props=>props.type==="filled" && "white" };
 
-const TopTexts = styled.div`
-`
+`;
+
+const TopTexts = styled.div``;
 
 const TopText = styled.span`
-margin: 0px 10px;
-text-decoration: underline;
-cursor: pointer;
+  margin: 0px 10px;
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 const Bottom = styled.div``;
-
 
 const Cart = () => {
   return (
@@ -40,12 +46,12 @@ const Cart = () => {
       <Wrapper>
         <Title>YOUR BAG</Title>
         <Top>
-          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopButton type="abc">CONTINUE SHOPPING</TopButton>
           <TopTexts>
             <TopText>Shopping Bag(2)</TopText>
             <TopText>Your Whishlist</TopText>
           </TopTexts>
-            <TopButton>CHECKOUT NOW</TopButton>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
         <Bottom></Bottom>
       </Wrapper>
