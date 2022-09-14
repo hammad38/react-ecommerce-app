d894648c3000df0c65bd029312702e74b626d2c7
obenstein <hammadsami119@gmail.com>
2022-09-14 20:13:56 +0500

    .

--- src/component/Announcement.jsx
+++ src/component/Announcement.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 30px;
-  background-color: teal;
+  background-color: black;
   color: white;
   display: flex;
   justify-content: center;
