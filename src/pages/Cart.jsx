4b66fce06d6ed75389e6a7367f1341c48e609831
obenstein <hammadsami119@gmail.com>
2022-09-14 16:28:35 +0500

    .

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,32 +1,48 @@
-import styled from "styled-components"
-import Announcement from "../component/Announcement"
-import Footer from "../component/Footer"
-import Navbar from "../component/Navbar"
+import styled from "styled-components";
+import Announcement from "../component/Announcement";
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
 
-const Container = styled.div`  
- `
+const Container = styled.div``;
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Title = styled.h1`
+padding: 20px;
+`;
+
+const Top = styled.div`
 display: flex;
-justify-content: center;
-`
-const Title = styled.h1``
-const Top = styled.div``                 
-const Bottom = styled.div``
+align-items: center;
+justify-content: space-between;
+`;
+const TopButton = styled.button``;
+const TopTexts = styled.div``;
+const TopText = styled.span``;
+const Bottom = styled.div``;
 
 
 const Cart = () => {
   return (
     <Container>
-        <Navbar />
-        <Announcement />
-        <Wrapper>
-            <Title>YOUR BAG</Title>
-            <Top></Top>
-            <Bottom></Bottom>
-        </Wrapper>
-        <Footer />
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Whishlist</TopText>
+            <TopButton>CHECKOUT NOW</TopButton>
+          </TopTexts>
+        </Top>
+        <Bottom></Bottom>
+      </Wrapper>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default Cart 
+export default Cart;
