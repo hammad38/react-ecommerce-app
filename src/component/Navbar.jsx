ca33ca5fb7238cabb8f77ddb9ea6a39c0059ba67
hammad38 <hammadsami119@gmail.com>
2022-09-16 17:06:08 +0500

    .

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -37,6 +37,7 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
+  ${mobile({marginRight:"4px"})}
 `;
 
 const Input = styled.input`
--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -1,5 +1,6 @@
 import { Send } from '@material-ui/icons'
 import styled from 'styled-components'
+import { mobile } from '../Responsive'
 
 const Container = styled.div`
 height: 60vh;
@@ -11,11 +12,13 @@ flex-direction: column;
 `
 const Title = styled.h1`
 font-size: 70px;
+${mobile({fontSize:"50px"})}
 `
 const Description = styled.div`
 font-weight: 300;
 font-size: 24px;
 margin: 25px;
+${mobile({textAlign: "center", fontSize:"20px"})}
 `
 
 const InputContainer = styled.div`
@@ -25,6 +28,7 @@ background-color: #fff;
 display: flex;
 justify-content: space-between;
 border: 1px solid gray;
+${mobile({width: "80%"})}
 
 `
 const Input = styled.input`
